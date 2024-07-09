d8fc641fccba97e5a3d48b98ca1c126317caac7b
suphaloek66 <141808829+suphaloek66@users.noreply.github.com>
2024-07-09 17:48:33 +0700

    complete(i think)

--- q2.ts
+++ q2.ts
@@ -1,14 +1,22 @@
 // const axios = require("axios");
 import axios from "axios";
+import { todo } from "node:test";
 
 /* assign interface/type to the function definition properly */
-const getTodo = async (todoId: number) => {
-  const result = await axios.get(`https://jsonplaceholder.typicode.com/todos/${todoId}`).then((result) => {
-    return `${result.data.owner} ${result.data.title} ${result.data.completed}`;
-  }).catch((err) =>{return "INVALID TODO ID"});
-return result;
-  /* Your code here */
+const getTodo = async (todoId: number) => { try{
+  const todo = await axios.get(`https://jsonplaceholder.typicode.com/todos/${todoId}`);
+  try{
+    const number_user = todo.data.userId;
+    const owner = await axios.get(`https://jsonplaceholder.typicode.com/users/${number_user}`)
+    const return_result = {
+      "owner ":  `${owner.data.name}`,
+      "Title ":  `${todo.data.title}`,
+      "Complete ":  `${todo.data.completed}`
+    }; return return_result;
+  }catch(err) {return "INVALID TODO ID"}
+}catch(err) {return "INVALID TODO ID"}
 };
+  /* Your code here */
 
 //test case
 const input1 = 15;
